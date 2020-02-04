20a2aba6830d955d3e8044e79aa684c46c8e624c
OH YUNHOO <40500484+chalchichi@users.noreply.github.com>
2020-02-04 15:12:47 +0900

    Create test2(dnn).py

---
+++ test2(dnn).py
@@ -0,0 +1,53 @@
+#########
+# 신경망 모델 구성
+######
+X = tf.placeholder(tf.float32, shape=[None, 26*16])
+Y = tf.placeholder(tf.float32, shape=[None, 16])
+W1 = tf.Variable(tf.random_uniform([26*16, 26*16], -1., 1.))
+ 
+# b1은 편향(bias)이며, 첫 번째 히든 레이어의 뉴런 갯수입니다. 
+b1 = tf.Variable(tf.zeros([26*16]))
+ 
+# 신경망의 히든 레이어에 가중치 W1과 편향 b1을 적용합니다
+L1 = tf.nn.relu(tf.add(tf.matmul(X, W1), b1))
+ 
+ 
+W2 = tf.Variable(tf.random_normal([26*16,20*10]))
+b2 = tf.Variable(tf.zeros([200]))
+L2 = tf.nn.relu(tf.add(tf.matmul(L1, W2), b2))
+ 
+W3 = tf.Variable(tf.random_normal([200, 150]))
+b3 = tf.Variable(tf.zeros([150]))
+L3 = tf.nn.relu(tf.add(tf.matmul(L2, W3), b3))
+ 
+W4 = tf.Variable(tf.random_normal([150, 100]))
+b4 = tf.Variable(tf.zeros([100]))
+L4 = tf.nn.relu(tf.add(tf.matmul(L3, W4), b4))
+ 
+W5 = tf.Variable(tf.random_normal([100 , 16]))
+b5 = tf.Variable(tf.zeros([16]))
+L5 = tf.nn.relu(tf.add(tf.matmul(L4, W5), b5))
+ 
+W6 = tf.Variable(tf.random_normal([16, 16]))
+b6 = tf.Variable(tf.zeros([16]))
+model = tf.add(tf.matmul(L5, W6), b6)
+cost = tf.reduce_mean(
+    tf.nn.softmax_cross_entropy_with_logits_v2(labels=Y, logits=model))
+ 
+optimizer = tf.train.AdamOptimizer(learning_rate=0.01)
+train_op = optimizer.minimize(cost)
+init = tf.global_variables_initializer()
+sess = tf.Session()
+sess.run(init)
+ 
+for step in range(10000):
+    sess.run(train_op, feed_dict={X: x_data, Y: y_data})
+prediction = tf.argmax(model, 1)
+target = tf.argmax(Y, 1)
+print('예측값:', sess.run(prediction, feed_dict={X: x_data}))
+print('실제값:', sess.run(target, feed_dict={Y: y_data}))
+ 
+is_correct = tf.equal(prediction, target)
+accuracy = tf.reduce_mean(tf.cast(is_correct, tf.float32))
+print('정확도: %.2f' % sess.run(accuracy * 100, feed_dict={X: x_data, Y: y_data}))
+sess.close()
